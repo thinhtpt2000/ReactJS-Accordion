5669042ae062850bd890cae06de35ee683903413
thinhtpt2000 <tthinh2128@gmail.com>
2020-07-30 11:00:20 +0700

    add on click heading to collapse

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,19 @@ import React, { useState } from "react";
 const Accordion = (props) => {
   const [isCollapsed, setCollapsed] = useState(true);
   const { heading, content } = props;
+
+  const handleHeadingClick = () => {
+    setCollapsed(!isCollapsed);
+  };
+
   return (
     <div className="Accordion">
-        <h2 className="Heading">{heading}</h2>
-        <div className="Content">{content}</div>
+      <h2 className="Heading" onClick={handleHeadingClick}>
+        {heading}
+      </h2>
+      {!isCollapsed && <div className="Content">{content}</div>}
     </div>
-  )
+  );
 };
 
 export default Accordion;
